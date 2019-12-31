731f6f97d2dd2d5303e4311ee652f7e0e9eceb3a
Administrator <573087829@qq.com>
2019-12-31 16:59:29 +0800

    测试

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-    @RequestMapping("/test")
+    @RequestMapping("/1")
     public String c(){
 
         return "myPage";
